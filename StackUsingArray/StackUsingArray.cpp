819d32e350134d06af097e4ee192d36cb4559a7a
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-06-16 10:48:45 +0700

    code untuk push

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -14,7 +14,21 @@ public :
         top = -1;
     }
 
-    int push
+    int push(int element) {
+        if (top == 4) { // step 1
+            cout << "Number of data exceeds the limit." << endl;
+            return 0;
+        }
+
+        top++;
+        stack_array[top] = element;                                     //step 3
+        cout << endl;
+        cout << element << "ditambahkan(pushed)" << endl;
+
+        return element;
+    }
+
+
 };
 
 int main()
